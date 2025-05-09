1b230e6a5abce9ebe5acdbf0dfc5308749d9df59
0xAhmd <ahmedhesham200987@gmail.com>
2025-05-09 16:40:52 +0300

    Implement drawer and settings page components with theme toggle functionality

---
+++ lib/components/drawer.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:wallgram/components/drawer_list_tile.dart';
+import 'package:wallgram/pages/settings_page.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 50.0),
+                child: Image.asset('assets/wall.png', width: 100),
+              ),
+              Divider(color: Theme.of(context).colorScheme.secondary),
+              const SizedBox(height: 10),
+              DrawerListTile(
+                icon: Icons.home,
+                title: 'H O M E',
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              DrawerListTile(
+                icon: Icons.settings,
+                title: 'S E T T I N G S',
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SettingsPage(),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/settings_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:wallgram/components/settings_list_tile.dart';
+import 'package:wallgram/themes/theme_provider.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('S E T T I N G S', style: TextStyle(fontSize: 26)),
+        foregroundColor: Theme.of(context).colorScheme.primary,
+      ),
+
+      body: Column(
+        children: [
+          MySettingsListTile(
+            title: 'Dark Mode',
+            action: CupertinoSwitch(
+              value: Provider.of<ThemeProvider>(context).isDarkMode,
+              onChanged: (value) {
+                Provider.of<ThemeProvider>(
+                  context,
+                  listen: false,
+                ).toggleTheme();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
