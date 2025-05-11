d6cc30ab0d299e1b9c9f59847f54c46dd61cc6c3
0xAhmd <ahmedhesham200987@gmail.com>
2025-05-11 21:51:54 +0300

    firebase setup done

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:wallgram/components/custom_text_field.dart';
+import 'package:wallgram/components/my_custom_button.dart';
+import 'package:wallgram/pages/register_page.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+  static const String routeName = 'login_page';
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 18),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.lock_open,
+                  size: 100,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  'Welcome Back, you\'ve been missed',
+                  style: TextStyle(
+                    fontSize: 19,
+                    color: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                CustomTextField(
+                  controller: _emailController,
+                  hintText: 'Enter your Email',
+                  icon: Icons.email,
+                  obscureText: false,
+                ),
+                const SizedBox(height: 12),
+                CustomTextField(
+                  controller: _passwordController,
+                  hintText: 'Enter your Password',
+                  icon: Icons.password,
+                  obscureText: true,
+                ),
+                const SizedBox(height: 10),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Forget Password?",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 25),
+                MyCustomButton(
+                  text: 'Login',
+                  onPressed: () {
+                    // Handle login logic
+                  },
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Don\'t have an account?',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                    ),
+                    const SizedBox(width: 2),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, RegistierPage.routeName);
+                      },
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 17,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
