1f09aeeb1e54a9d235519fafb50d0c97ec217fd8
0xAhmd <ahmedhesham200987@gmail.com>
2025-05-11 22:00:04 +0300

    firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:wallgram/firebase_options.dart';
 import 'package:wallgram/pages/login_page.dart';
 import 'package:wallgram/pages/register_page.dart';
 import 'package:wallgram/themes/theme_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     ChangeNotifierProvider(
       create: (context) => ThemeProvider(),
