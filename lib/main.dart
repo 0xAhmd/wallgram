51cb58876e50b4d207ca576330d6ac4e9aa9385d
0xAhmd <ahmedhesham200987@gmail.com>
2025-05-13 01:50:02 +0300

    enable device_previw

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -18,7 +19,7 @@ void main() async {
         ChangeNotifierProvider(create: (context) => ThemeProvider()),
         ChangeNotifierProvider(create: (context) => DatabaseProvider()),
       ],
-      child: const MyApp(),
+      child: DevicePreview(builder: (context) => const MyApp()),
     ),
   );
 }
