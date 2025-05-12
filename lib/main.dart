e5864e54f3fce8847ea3dcf11d09578502f54664
0xAhmd <ahmedhesham200987@gmail.com>
2025-05-12 22:13:19 +0300

    Refactor main.dart to use MultiProvider for ThemeProvider and DatabaseProvider; remove unused SystemChrome settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,23 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:wallgram/firebase_options.dart';
 import 'package:wallgram/pages/home_page.dart';
 import 'package:wallgram/pages/login_page.dart';
 import 'package:wallgram/pages/register_page.dart';
 import 'package:wallgram/services/auth/auth_gate.dart';
+import 'package:wallgram/services/database/database_provider.dart';
 import 'package:wallgram/themes/theme_provider.dart';
 
 void main() async {
-  SystemChrome.setSystemUIOverlayStyle(
-    SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      systemNavigationBarColor: Colors.grey.shade300,
-    ),
-  );
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
-    ChangeNotifierProvider(
-      create: (context) => ThemeProvider(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => ThemeProvider()),
+        ChangeNotifierProvider(create: (context) => DatabaseProvider()),
+      ],
       child: const MyApp(),
     ),
   );
