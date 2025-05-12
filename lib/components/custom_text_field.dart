c3a02fa213f50c5ed9942d48bb4780c1a9a78fcc
0xAhmd <ahmedhesham200987@gmail.com>
2025-05-12 14:07:38 +0300

    added suffex icon can toggle password

--- lib/components/custom_text_field.dart
+++ lib/components/custom_text_field.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class CustomTextField extends StatelessWidget {
+class CustomTextField extends StatefulWidget {
   const CustomTextField({
     super.key,
     required this.hintText,
@@ -13,14 +13,47 @@ class CustomTextField extends StatelessWidget {
   final String hintText;
   final IconData icon;
   final bool obscureText;
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  late bool _isObscured;
+
+  @override
+  void initState() {
+    super.initState();
+    _isObscured = widget.obscureText;
+  }
+
+  void _toggleObscureText() {
+    setState(() {
+      _isObscured = !_isObscured;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return TextField(
-      controller: controller,
-      obscureText: obscureText,
+      controller: widget.controller,
+      obscureText: _isObscured,
       decoration: InputDecoration(
-        hintText: hintText,
-        prefixIcon: Icon(icon, color: Theme.of(context).colorScheme.primary),
+        hintText: widget.hintText,
+        prefixIcon: Icon(
+          widget.icon,
+          color: Theme.of(context).colorScheme.primary,
+        ),
+        suffixIcon:
+            widget.obscureText
+                ? IconButton(
+                  icon: Icon(
+                    _isObscured ? Icons.visibility : Icons.visibility_off,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                  onPressed: _toggleObscureText,
+                )
+                : null,
         hintStyle: TextStyle(color: Theme.of(context).colorScheme.primary),
         filled: true,
         fillColor: Theme.of(context).colorScheme.secondary,
