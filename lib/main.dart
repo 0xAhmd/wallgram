1fb0eaa29c38d00cc1d6f23711213051e05317b5
0xAhmd <ahmedhesham200987@gmail.com>
2025-05-17 01:04:03 +0300

    downgrade intl and fix space betweeen comment and counter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -22,7 +21,7 @@ void main() async {
         ChangeNotifierProvider(create: (context) => ThemeProvider()),
         ChangeNotifierProvider(create: (context) => DatabaseProvider()),
       ],
-      child: DevicePreview(builder: (context) => const MyApp()),
+      child: const MyApp(),
     ),
   );
 }
