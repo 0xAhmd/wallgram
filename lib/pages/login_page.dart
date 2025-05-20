3f1185d8be0e5fd8bc0312bce464e65ceee90897
0xAhmd <ahmedhesham200987@gmail.com>
2025-05-20 11:51:38 +0300

    keyboard overflow

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -96,97 +96,114 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
       body: SafeArea(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 18),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.lock_open,
-                  size: 100,
-                  color: Theme.of(context).colorScheme.primary,
-                ),
-                const SizedBox(height: 20),
-                Text(
-                  'Welcome Back, you\'ve been missed',
-                  style: TextStyle(
-                    fontSize: 19,
-                    color: Theme.of(context).colorScheme.primary,
-                    fontWeight: FontWeight.bold,
+        child: LayoutBuilder(
+          builder:
+              (context, constraints) => SingleChildScrollView(
+                padding: const EdgeInsets.symmetric(horizontal: 18),
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                  child: IntrinsicHeight(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(height: 30),
+                        Icon(
+                          Icons.lock_open,
+                          size: 100,
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                        const SizedBox(height: 20),
+                        Text(
+                          'Welcome Back, you\'ve been missed',
+                          style: TextStyle(
+                            fontSize: 19,
+                            color: Theme.of(context).colorScheme.primary,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        CustomTextField(
+                          controller: _emailController,
+                          hintText: 'Enter your Email',
+                          icon: Icons.email,
+                          obscureText: false,
+                        ),
+                        const SizedBox(height: 12),
+                        CustomTextField(
+                          controller: _passwordController,
+                          hintText: 'Enter your Password',
+                          icon: Icons.password,
+                          obscureText: true,
+                        ),
+                        const SizedBox(height: 25),
+                        MyCustomButton(text: 'Login', onPressed: login),
+                        const SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Don\'t have an account?',
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Theme.of(context).colorScheme.primary,
+                              ),
+                            ),
+                            const SizedBox(width: 2),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pushNamed(
+                                  context,
+                                  RegisterPage.routeName,
+                                );
+                              },
+                              child: Text(
+                                'Sign Up',
+                                style: TextStyle(
+                                  color: Theme.of(context).colorScheme.primary,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 17,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Divider(
+                                color: Colors.grey[400],
+                                thickness: 0.5,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 10.0,
+                              ),
+                              child: Text(
+                                'Or Sign in With',
+                                style: TextStyle(color: Colors.grey[700]),
+                              ),
+                            ),
+                            Expanded(
+                              child: Divider(
+                                color: Colors.grey[400],
+                                thickness: 0.5,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 24),
+                        CustomSquareTile(
+                          img: 'assets/google.png',
+                          onTap: () => _signInWithGoogle(context),
+                        ),
+                        const SizedBox(height: 24),
+                      ],
+                    ),
                   ),
                 ),
-                const SizedBox(height: 20),
-                CustomTextField(
-                  controller: _emailController,
-                  hintText: 'Enter your Email',
-                  icon: Icons.email,
-                  obscureText: false,
-                ),
-                const SizedBox(height: 12),
-                CustomTextField(
-                  controller: _passwordController,
-                  hintText: 'Enter your Password',
-                  icon: Icons.password,
-                  obscureText: true,
-                ),
-               
-                const SizedBox(height: 25),
-                MyCustomButton(text: 'Login', onPressed: login),
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Don\'t have an account?',
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Theme.of(context).colorScheme.primary,
-                      ),
-                    ),
-                    const SizedBox(width: 2),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, RegistierPage.routeName);
-                      },
-                      child: Text(
-                        'Sign Up',
-                        style: TextStyle(
-                          color: Theme.of(context).colorScheme.primary,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 17,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Divider(color: Colors.grey[400], thickness: 0.5),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Text(
-                        'Or Sign in With',
-                        style: TextStyle(color: Colors.grey[700]),
-                      ),
-                    ),
-                    Expanded(
-                      child: Divider(color: Colors.grey[400], thickness: 0.5),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 24),
-                CustomSquareTile(
-                  img: 'assets/google.png',
-                  onTap: () => _signInWithGoogle(context),
-                ),
-                const SizedBox(width: 24),
-              ],
-            ),
-          ),
+              ),
         ),
       ),
     );
